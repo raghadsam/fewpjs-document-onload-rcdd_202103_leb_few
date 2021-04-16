ed47d94e3ac97d46d3e4da2d0cd996d7c2e941e6
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-04-16 19:57:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
+<<<<<<< HEAD
 document.getElementById("text").textContent="This is really cool!";
 });
+=======
+  console.log("The DOM has loaded");
+});
+document.getElementById("text").textContent="This is very cool!";
+>>>>>>> b109d57de83a807640f6643d40af9de01f434d92
